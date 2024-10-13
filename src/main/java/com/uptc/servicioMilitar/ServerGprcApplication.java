8aa4d25eccbfbaee067663fa352c96a2805072b2
JuanManuel1207 <juan.lopez14@uptc.edu.co>
2024-10-13 18:32:07 -0500

    añadir archivos de configuración

---
+++ src/main/java/com/uptc/servicioMilitar/ServerGprcApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.servicioMilitar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGprcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGprcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/servicioMilitar/ServerGprcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.servicioMilitar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGprcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
