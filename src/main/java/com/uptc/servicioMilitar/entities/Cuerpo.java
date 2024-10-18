95a34af65b9b542632c0ca704bba9f7253a69b68
Laura Perez <lauralicethperezrodriguez@gmail.com>
2024-10-17 20:48:33 -0500

    Actualizacion de las entidades Servicio y Cuerpo modelo gRPC

--- src/main/java/com/uptc/servicioMilitar/entities/Cuerpo.java
+++ src/main/java/com/uptc/servicioMilitar/entities/Cuerpo.java
@@ -5,40 +5,20 @@ import java.util.List;
 
 @Entity
 @Table(name = "CUERPO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cuerpo {
 
     @Id
-    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "DENOMINACION")
     private String denominacion;
+    private Integer status;
 
+    /*
     @OneToMany(mappedBy = "cuerpo")
     private List<Soldado> soldados;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getDenominacion() {
-        return denominacion;
-    }
-
-    public void setDenominacion(String denominacion) {
-        this.denominacion = denominacion;
-    }
-
-    @Override
-    public String toString() {
-        return "Cuerpo{" +
-                "id=" + id +
-                ", denominacion='" + denominacion + '\'' +
-                ", soldados=" + soldados +
-                '}';
-    }
+    */
 }
+
